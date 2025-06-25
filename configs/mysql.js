10615018b28b137a2338183e262f0d86bec1a549
CarlosPriego <aceytunocarlos897@hotmail.com>
2025-06-25 17:37:12 -0600

    Cambios en la conexion con la base de datos

--- configs/mysql.js
+++ configs/mysql.js
@@ -1,9 +1,10 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("DBConcesionaria", "root", "admin", {
-  host: "localhost",
+export const sequelize = new Sequelize("DBConcesionaria", "root", "vFcOySaeonRXYpFgipFVyzmGUACappCN", {
+  host: "tramway.proxy.rlwy.net",
   dialect: "mysql",
-  port: process.env.PORTMYSQL || 3306,
+  port: process.env.PORTMYSQL || 10373,
 });
 
 // sequelize requiere que ya se halla creado una base de datos
+//mysql://root:vFcOySaeonRXYpFgipFVyzmGUACappCN@tramway.proxy.rlwy.net:10373/railway
